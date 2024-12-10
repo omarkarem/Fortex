6008d71e1e114c5a100805989ebd0865cb1a78c3
omarkarem <omar_karem156@outlook.com>
2024-12-10 22:56:26 +0200

    fixed routes

--- server/routes/PaymentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createPaymentIntent } from '../controllers/paymentController.js';
-
-const router = express.Router();
-
-// POST /api/payment-intent - Create a Payment Intent
-router.post('/payment-intent', createPaymentIntent);
-
-export default router;
