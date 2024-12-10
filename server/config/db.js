4ee524b4b9cdfbe3a5310ba866105ab0724f7a9c
omarkarem <omar_karem156@outlook.com>
2024-12-10 22:01:08 +0200

    db new

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,16 @@ import mongoose from "mongoose";
 
 // MongoDB connection
 const connectDB = async () => {
-    try {
-      await mongoose.connect(process.env.MONGODB_URI);
-      console.log('MongoDB connected');
-    } catch (err) {
-      console.error("MongoDB connection failed", err);
-    }
-  };
+  try {
+      await mongoose.connect(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      });
+      console.log("MongoDB connected...");
+  } catch (error) {
+      console.error("MongoDB connection error:", error);
+      process.exit(1);
+  }
+};
 
 export default connectDB;
