d3cb700092be9b8e4bc6b02f6fb734e72f0c3ef8
omarkarem <omar_karem156@outlook.com>
2024-12-10 15:05:19 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: "http://localhost:3000", // Frontend origin
+    origin: "https://fortex-llc.vercel.app", // Frontend origin
     credentials: true,
   }));
   
