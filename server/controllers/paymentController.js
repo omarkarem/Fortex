91141b391c10ccd1eb0312405befa183101083ce
omarkarem <omar_karem156@outlook.com>
2024-12-11 03:35:41 +0200

    Update paymentController.js

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
 import User from '../models/User.js';
 import Property from '../models/Property.js';
+import mongoose from "mongoose";
+
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const createCheckoutSession = async (req, res) => {
